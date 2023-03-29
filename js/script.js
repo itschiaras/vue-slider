318f76a66f31a2992e984119e95fa94a994dcd6f
ChiaraS97 <chiara.sucato97@gmail.com>
2023-03-29 15:11:41 +0200

    add template to html, add img folder

--- js/script.js
+++ js/script.js
@@ -25,7 +25,8 @@ createApp({
                     title: "Marvel's Avengers",
                     text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
                 }
-            ]
+            ],
+            activeIndex: 0
         }
     },
 
